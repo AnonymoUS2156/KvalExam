3b337dcc5d79680c1de29d0aa613b2d5c331abb1
AnonymoUS2156 <48409221+AnonymoUS2156@users.noreply.github.com>
2023-07-05 12:06:21 +0300

    Add files via upload

---
+++ ClassLibrary3/ClassLibrary3/Apartment.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary3
+{
+    //Грибков Никита 31 ИС
+    public class Apartment
+    {
+        public int Number { get; set; } //поле Номер
+        public string Owner { get; set; } //поле Владелец
+
+        private List<Room> Rooms = new List<Room>(); // список Комнат
+
+        public void AddApps(Room rooms) // метод добавления комнат 
+        {
+            Rooms.Add(rooms);// добавление комнат
+        }
+        public void ShowAll()// показать все
+        {
+            Rooms.Sort(); //сортировка по первым значениям
+            foreach (Room room in Rooms)//для каждой комнате в списке...
+                room.Show();//вывести значение и площадь
+        }
+    }
+    public class Room : IComparable<Room> //Класс комнаты вместе с интерфейсом сравнения CompareTo() (интерфейс сделан внутри класса для уменьшения затрат времени)
+    {
+        public string Name { get; set; } //поле Название
+        public int Area { get; set; }// поле Площадь
+        public int CompareTo(Room other) //метод сравнения 
+        {
+            if (Name.CompareTo(other.Name) != 0) //Если буква в названии 
+                return Name.CompareTo(other.Name);//вывести первое попавшееся
+            else //или
+                return Area.CompareTo(other.Area);//вернуть значения сравнения с площадью
+        }
+        public void Show()//вывести
+        {
+            Console.WriteLine("Назначение: {0}, Площадь: {1}", Name, Area);//выводится это
+        }
+
+
+    }
+}
---
+++ ClassLibrary3/KvalExamGribkov/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ClassLibrary3;
+
+namespace KvalExamGribkov
+{
+    //Грибков Никита 31 ИС
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            Apartment apartment = new Apartment();//реализация класса
+            apartment.Number = 28; //кол-во номеров в апартаментах
+            apartment.Owner = "ОАО МосГорСтрой"; //владелеца апартаментов
+
+            
+
+
+            Room[] rooms = new Room[] //списов имеющихся комнат с характеристиками
+            {
+                new Room { Name = "4х комнатная", Area = 54}, //характеристики комнат
+                new Room { Name = "3х комнатная", Area = 36 },
+                new Room { Name = "1 комнатная", Area = 24 },
+                new Room { Name = "2х комнатная", Area = 32 },
+            };
+
+            foreach (Room room in rooms)
+                apartment.AddApps(room);//"переборка и вывод списка в консоль"
+
+            Console.WriteLine("Владелец: {0}, кол-во комнат: {1}",apartment.Owner , apartment.Number); //Вывод 
+            apartment.ShowAll();//вывод всех характеристик
+            Console.ReadKey();//ожидать нажатия кнопки
+        }
+    }
+}
